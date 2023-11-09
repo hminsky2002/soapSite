8e24314d08af962735e37331232ffeeb33896cc1
hminsky2002 <harry.minsky@gmail.com>
2023-11-08 22:17:51 -0500

    initial commit, added gif and logo

---
+++ soap-web-app/app/components/Spiny.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Spiny() {
+  return (
+    <div className="flex h-screen">
+      <div className="container m-auto w-full md:w-6/12">
+        <img src="/LOGO-Large.png" className="w-6/12 m-auto " alt="Logo"/>
+        <img src="/spinning-magazine.gif" alt="spinning soap mag" />
+      </div>
+    </div>
+  );
+}
---
+++ soap-web-app/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+import Spiny from './components/Spiny'
+export default function Home() {
+  return (
+    <main className="min-h-screen flex-col items-center justify-between">
+      <Spiny/>
+    </main>
+  )
+}
