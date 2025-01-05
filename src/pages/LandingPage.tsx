d3627ee7366a0806d16bb48ccb200966e4cba229
Harry Minsky <harry.minsky@gmail.com>
2025-01-04 19:11:19 -0500

    make it a react site with router

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,12 @@
+const LandingPage = () => {
+  return (
+    <>
+      <div className="mx-auto">
+        <img className="mx-auto w-1/2" src="LOGO-Large.png" alt="Logo" />
+        <img className="mx-auto w-1/2" src="spinning-magazine.gif" alt="Logo" />
+      </div>
+    </>
+  );
+};
+
+export { LandingPage };
