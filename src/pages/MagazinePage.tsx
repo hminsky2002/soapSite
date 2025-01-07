344d0e794a0f0accd0bfd12d45e7b6bbdeb3dd4c
Harry Minsky <harry.minsky@gmail.com>
2025-01-07 15:05:00 -0500

    remove alt text

--- src/pages/MagazinePage.tsx
+++ src/pages/MagazinePage.tsx
@@ -7,27 +7,27 @@ const MagazinePage = () => {
         {/* @ts-ignore */}
         <HTMLFlipBook className="mx-auto" width={600} height={800}>
           <p></p>
-          <img className="" loading="lazy" src="magImages/cover.png" alt="opener" />
+          <img className="" loading="lazy" src="magImages/cover.png" alt="" />
           {Array.from({ length: 90 }, (_, index) => (
             <img
               key={index + 1}
               src={`magImages/image_${index + 1}.png`}
-              alt={`page ${index + 1}`}
+              alt={``}
               loading="lazy"
             />
           ))}
-          <img className="" src="magImages/backcover.png" alt="finisher" />
+          <img className="" src="magImages/backcover.png" alt="" />
           <p></p>
         </HTMLFlipBook>
       </div>
       <div className=" mx-auto md:hidden">
-        <img loading="lazy" className="mx-auto w-1/2" src="magImages/cover.png" alt="opener" />
+        <img loading="lazy" className="mx-auto w-1/2" src="magImages/cover.png" alt="" />
         <div className="grid grid-cols-2">
           {Array.from({ length: 90 }, (_, index) => (
             <img
               key={index + 1}
               src={`magImages/image_${index + 1}.png`}
-              alt={`page ${index + 1}`}
+              alt={``}
               loading="lazy"
             />
           ))}
@@ -35,7 +35,7 @@ const MagazinePage = () => {
         <img
           className="mx-auto w-1/2"
           src="magImages/backcover.png"
-          alt="finisher"
+          alt=""
           loading="lazy"
         />
       </div>
