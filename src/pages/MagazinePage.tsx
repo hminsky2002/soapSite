43654ddbbeaf3969cf7c1a1ecba11312aa7f027d
Harry Minsky <harry.minsky@gmail.com>
2025-01-07 15:03:35 -0500

    add lazy loading

--- src/pages/MagazinePage.tsx
+++ src/pages/MagazinePage.tsx
@@ -7,12 +7,13 @@ const MagazinePage = () => {
         {/* @ts-ignore */}
         <HTMLFlipBook className="mx-auto" width={600} height={800}>
           <p></p>
-          <img className="" src="magImages/cover.png" alt="opener" />
+          <img className="" loading="lazy" src="magImages/cover.png" alt="opener" />
           {Array.from({ length: 90 }, (_, index) => (
             <img
               key={index + 1}
               src={`magImages/image_${index + 1}.png`}
               alt={`page ${index + 1}`}
+              loading="lazy"
             />
           ))}
           <img className="" src="magImages/backcover.png" alt="finisher" />
@@ -20,13 +21,14 @@ const MagazinePage = () => {
         </HTMLFlipBook>
       </div>
       <div className=" mx-auto md:hidden">
-        <img className="mx-auto w-1/2" src="magImages/cover.png" alt="opener" />
+        <img loading="lazy" className="mx-auto w-1/2" src="magImages/cover.png" alt="opener" />
         <div className="grid grid-cols-2">
           {Array.from({ length: 90 }, (_, index) => (
             <img
               key={index + 1}
               src={`magImages/image_${index + 1}.png`}
               alt={`page ${index + 1}`}
+              loading="lazy"
             />
           ))}
         </div>
@@ -34,6 +36,7 @@ const MagazinePage = () => {
           className="mx-auto w-1/2"
           src="magImages/backcover.png"
           alt="finisher"
+          loading="lazy"
         />
       </div>
     </div>
