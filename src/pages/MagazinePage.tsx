9b5532a217c7a5c6ed8ea6dde6fc9f3c6fa61e30
Harry Minsky <harry.minsky@gmail.com>
2025-01-07 01:25:28 -0500

    add flipbook

--- src/pages/MagazinePage.tsx
+++ src/pages/MagazinePage.tsx
@@ -1,16 +1,40 @@
+import HTMLFlipBook from "react-pageflip";
+
 const MagazinePage = () => {
   return (
     <div className="magbody">
-      <div className="mx-auto md:w-1/2">
-        <img className="mx-auto w-1/2" src="magImages/1png.png" alt="opener" />
-        {Array.from({ length: 45 }, (_, index) => (
-          <img
-            key={index + 2}
-            src={`magImages/${index + 2}png.png`}
-            alt={`page ${index + 2}`}
-          />
-        ))}
-        <img className="mx-auto w-1/2" src="magImages/47png.png" alt="finisher" />
+      <div className="hidden pt-8 md:block">
+        {/* @ts-ignore */}
+        <HTMLFlipBook className="mx-auto" width={600} height={800}>
+          <p></p>
+          <img className="" src="magImages/cover.png" alt="opener" />
+          {Array.from({ length: 90 }, (_, index) => (
+            <img
+              key={index + 1}
+              src={`magImages/image_${index + 1}.png`}
+              alt={`page ${index + 1}`}
+            />
+          ))}
+          <img className="" src="magImages/backcover.png" alt="finisher" />
+          <p></p>
+        </HTMLFlipBook>
+      </div>
+      <div className=" mx-auto md:hidden">
+        <img className="mx-auto w-1/2" src="magImages/cover.png" alt="opener" />
+        <div className="grid grid-cols-2">
+          {Array.from({ length: 90 }, (_, index) => (
+            <img
+              key={index + 1}
+              src={`magImages/image_${index + 1}.png`}
+              alt={`page ${index + 1}`}
+            />
+          ))}
+        </div>
+        <img
+          className="mx-auto w-1/2"
+          src="magImages/backcover.png"
+          alt="finisher"
+        />
       </div>
     </div>
   );
